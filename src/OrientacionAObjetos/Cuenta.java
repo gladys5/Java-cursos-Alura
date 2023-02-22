09c3ef5061a7c1848a6a4b0f395da18dbb6727c9
gladys5 <yumanji@live.com>
2023-02-22 01:39:37 -0700

    clase cuenta y metodos explicados

--- src/OrientacionAObjetos/Cuenta.java
+++ src/OrientacionAObjetos/Cuenta.java
@@ -1,7 +1,5 @@
 package OrientacionAObjetos;
 
-
-
 public class Cuenta {
 
     double saldo;
@@ -9,4 +7,25 @@ public class Cuenta {
     int numero;
     String titular;
 
+    public void deposita(double valor) {
+        this.saldo = this.saldo + valor;
+    }
+
+    public boolean saca(double valor) {
+        if(this.saldo >= valor) {
+            this.saldo -= valor;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean transfiere(double valor, Cuenta destino) {
+        if(this.saldo >= valor) {
+            this.saldo -= valor;
+            destino.deposita(valor);
+            return true;
+        }
+        return false;
+    }
 }
